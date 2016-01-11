9cdd9cdd907ce493791a4f07a5175500df3681fc
Erick <Erick@Erick.connectify>
2016-01-11 10:36:07 -0500

    GG

---
+++ ISENERO/src/isenero/ISENERO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isenero;
+
+/**
+ *
+ * @author Erick
+ */
+public class ISENERO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("pa k quieres saber eso jaja saludos.");
+    }
+    
+}
